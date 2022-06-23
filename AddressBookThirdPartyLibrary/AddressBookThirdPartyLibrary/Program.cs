726fbd8d1a41966b3dfce0d7464c5226a4259f79
PSUMIT1995 <psumit191.sp@gmail.com>
2022-06-23 20:25:09 +0530

    UC1 : Read And Write Into CSV file

--- AddressBookThirdPartyLibrary/AddressBookThirdPartyLibrary/Program.cs
+++ AddressBookThirdPartyLibrary/AddressBookThirdPartyLibrary/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+namespace AddressBookThirdPartyLibrary
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Address book");
+            //calling operation management class that contains operation
+            OperationManagement operationManagement = new OperationManagement();
+
+            //non-static method 
+            operationManagement.ReadInput();
+            Console.Read();
+        }
+    }
+}
